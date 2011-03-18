251534d764ad6a10d47484512b5e135d67bd18e9
Lars Kuhnt <lars@khnut.de>
2011-03-18 15:24:59 +0100

    only allow admins to set bienchen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.update_attributes(params[:user])
     @user.profile_updated_at = Time.now.utc
-    @user.avatar_image_url = params['bee'].blank? ? nil : '/images/bee.png'
+    @user.avatar_image_url = params['bee'].blank? || !current_user.admin? ? nil : '/images/bee.png'
     if @user.save
       redirect_to @user
     else
